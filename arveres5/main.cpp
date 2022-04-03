196c1d87710c2f3da48288f443fab5aed0e22992
Zsolt Sebestyén <sebestyn.zsolt@gmail.com>
2022-04-03 20:11:35 +0200

    nhf3

---
+++ arveres5/main.cpp
@@ -0,0 +1,109 @@
+/// Defines
+//#define TEST_MODE
+
+/// Includes
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <conio.h> // PressAKeyToContinue függvényhez
+#include <algorithm>
+#include <time.h> // Random szám generálás (srand)
+#include <unistd.h> // sleep()
+#include "memtrace.h"
+
+
+/// Saját fájlok
+#include "console.h"
+#include "console.cpp"
+#include "karakter.h"
+#include "karakter.cpp"
+#include "vevo.h"
+#include "vevo.cpp"
+#include "raktar.h"
+#include "raktar.cpp"
+#include "licitalas.h"
+#include "licitalas.cpp"
+#include "jatek.h"
+#include "jatek.cpp"
+
+/// Using namespaces
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+/// STATIKUS VÁLTOZÓK
+char* Jatek     ::KARAKTER_FAJL_NEV    = (char*)"karaker.dat";
+char* Jatek     ::JATEKOS_FAJL_NEV     = (char*)"jatekos.dat";
+int   Jatek     ::NYERO_EGYENLEG       = 100000;
+int   Ember     ::KEZDO_PENZ           = 25000;
+int   Licitalas ::KEZDO_AR             = 100;
+int   Raktar    ::MAX_KARAKTAREK_SZAMA = 8;
+int   Raktar    ::MIN_KARAKTAREK_SZAMA = 2;
+
+
+/// MAIN
+int main(){
+
+#ifdef TEST_MODE
+    cout << "==== TEST MODE ====" << endl << endl;
+    char bemenet = '0';
+    while(bemenet != 'q'){
+        cout << endl;
+        cout << "1 -> fajlok" << endl;
+        cout << "2 -> Raktar" << endl;
+        cout << "q -> kilepes" << endl;
+        cout << endl << "Valassz tesztet: ";
+        cin >> bemenet;
+
+        switch(bemenet){
+            case '1':
+                test::fajlok();
+                break;
+            case '2':
+                test::raktar();
+                break;
+
+        }
+    }
+
+#else
+
+    // Random generátor seed véletlet(idő) beállítása
+    srand(time(0));
+
+    /// Fő játék változó
+    Jatek Fojatek;
+
+    /// Előkészítés -> fájlok beolvasasa (hiba esetén kilép)
+    try{
+        Fojatek.beolvas_fajlokbol();
+    } catch(int){
+        cerr << "ERROR: Olyan hiba tortent ami miatt a program nem tud tovabb futni." << endl;
+        return -1;
+    }
+
+    /// Menü futtatasa  (hiba esetén kilép)
+    try{
+        Fojatek.fomenu_inditas();
+    } catch(int){
+        cerr << "ERROR: Olyan hiba tortent ami miatt a program nem tud tovabb futni." << endl;
+        return -1;
+    }
+
+    /// Fájlokba kiírás (hiba esetén kilép)
+    cout << " Fajlok mentese: ";
+    try{
+        Fojatek.kiiras_fajlba();
+        cout << "SIKERES" << endl;
+    } catch(int){
+        cout << "SIKERTELEN" << endl;
+    }
+
+    return 0;
+
+#endif // TEST_MODE
+}
+
+
+
---
+++ arveres_2022-04-03-kesz/adat_strukt/raktar.cpp
@@ -0,0 +1,64 @@
+
+#include "raktar.h"
+
+/// Konstruktor
+Raktar::Raktar(Karakter* el_kar, int el_kar_szama){
+
+    // Karakterek számának beállítása
+    this->kar_szama = random::range(MIN_KARAKTAREK_SZAMA, MAX_KARAKTAREK_SZAMA);
+
+    // Elérhető karakterek keverése
+    random_shuffle(&el_kar[0], &el_kar[el_kar_szama]);
+
+    // Karakterek tom lefoglalása
+    this->karakterek = new Karakter[this->kar_szama];
+
+    // Karakterek beállítása valószínűség alapján
+    int hanyadik = 0;
+    while(hanyadik < this->kar_szama){
+        int percent = random::range(1,100);
+        int i = random::range(0,el_kar_szama-1);
+        if(el_kar[i].get_gyakorisag() >= percent){
+            this->karakterek[hanyadik] = el_kar[i];
+            this->ossz_ertek += el_kar[i].get_ertek();
+            hanyadik++;
+        }
+
+    }
+
+}
+
+/// Kiírása
+void Raktar::print(){
+
+    // Raktar merete (x*x)
+    int x = sqrt(this->kar_szama) + 1;
+    if(x<3) x = 3;
+
+    // Üres raktár helyek kitötlése ' ' karakterrel
+    char karakterek_k[x*x];
+    for(int j=0; j<kar_szama; j++){
+        karakterek_k[j] = karakterek[j].get_k();
+    }
+    for(int j=kar_szama; j<(x*x); j++){
+        karakterek_k[j] = ' ';
+    }
+    //std::random_shuffle(&karakterek_k[0], &karakterek_k[x*x]);
+
+    // Kirajzolás
+    int i = 0;
+    for(int sor = 0; sor<x; sor++){
+        cout << setw(18) << " +---+";
+        for(int a=0; a<x-1; a++){cout << "---+";}
+        cout << endl << setw(12) << "";
+        for(int oszlop = 0; oszlop<x;oszlop++){
+            cout << " | " << karakterek_k[i++];
+        }
+        cout << " |" << endl;
+    }
+
+    cout << setw(18) <<  " +---+";
+    for(int a=0; a<x-1; a++){cout << "---+";}
+    cout << endl << endl;
+}
+
